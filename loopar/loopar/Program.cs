740fb655a84cddcb29e525efc59a289618fc106a
ullvirobertdeaconescu <31400164+ullvirobertdeaconescu@users.noreply.github.com>
2017-10-02 09:37:54 +0200

    Fragesport + matematik
    Frågesporten och diverse matematiska program.

---
+++ loopar/loopar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace loopar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            char forsokigen = "y";
+            while (forsokigen == "y")
+            {
+                Console.WriteLine("När jag är ung...");
+                string svar = Console.ReadLine();
+
+                if (svar.ToLower() == "människan")
+                {
+                    Console.WriteLine("rätt!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("vill du forsoka igen?");
+                    forsokigen = Console.ReadLine()[0];
+                }    
+           }
+        }   
+
+    }
+}
